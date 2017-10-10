311396a9b8c00b0ecf1efda7eb062cf2e05f59e1
viviting2007 <32618825+viviting2007@users.noreply.github.com>
2017-10-10 19:13:18 +0800

    Add files via upload

---
+++ our_environments.R
@@ -0,0 +1,8 @@
+list(
+  Fish = "x86_64-w64-mingw32/x64 (64-bit)",
+  Lisa = "x86_64-w64-mingw32/x64 (64-bit)",
+  Brian = "x86_64-w64-mingw32/x64 (64-bit)",
+  York = "x86_64-w64-mingw32/x64 (64-bit)",
+  Stanley = "x86_64-apple-darwin13.4.0 (64-bit)"
+)
+
