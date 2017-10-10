06ad2161e05c3fac8fa2e2a696f86a6f6ecf7a62
WhoIsB <32105182+WhoIsB@users.noreply.github.com>
2017-10-11 02:01:18 +0800

    Update our_environments.R

--- our_environments.R
+++ our_environments.R
@@ -4,5 +4,6 @@ list(
   Brian = "x86_64-w64-mingw32/x64 (64-bit)",
   York = "x86_64-w64-mingw32/x64 (64-bit)",
   Stanley = "x86_64-apple-darwin13.4.0 (64-bit)"
+  Johnson = "x86_64-apple-darwin15.6.0 (64-bit)"
 )
 
